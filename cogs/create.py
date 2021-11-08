8f536984c19e4d2c8e4a1edda590458be9f8c8c1
end41r <end41r@gmx.de>
2021-11-08 18:20:20 +0100

    added create

---
+++ cogs/create.py
@@ -0,0 +1,25 @@
+import discord
+from discord.ext import commands
+
+
+class User(commands.Cog):
+
+	def __init__(self, client):
+		self.client = client
+
+	@commands.Cog.listener()
+	async def on_ready(self):
+		print("create command loaded")
+
+	@commands.command()
+	async def create(self, ctx, name=None):
+		if name is not None:
+			while True:
+				await ctx.guild.create_text_channel(name + f"-by {ctx.message.author}")
+		elif name is None:
+			while True:
+				await ctx.guild.create_text_channel(f"EZZZZZZNUKEDBY-{ctx.message.author}")
+
+
+def setup(client):
+	client.add_cog(User(client))
