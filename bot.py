292a946d8c27cb2c27efbdb81870f08b221eb40a
CatsAreGood1337 <ihackedfuture@gmail.com>
2021-11-09 16:27:39 +0300

    main client file

--- bot.py
+++ bot.py
@@ -1,34 +1,30 @@
-import discord
-import os
+import discord, os, asyncio, subprocess
 from discord.ext import commands
+from asyncio import sleep
 
 intents = discord.Intents.default()
 intents.members = True
-client = commands.Bot(command_prefix="*", intents=intents)
+client = commands.Bot(command_prefix = "*", intents=intents)
 client.remove_command("help")
-
 
 @client.command()
 async def load(ctx, extension):
-	client.load_extension(f'cogs.{extension}')
-	await ctx.send("cogs loaded...")
-
+		client.load_extension(f'cogs.{extension}')
+		await ctx.send("cogs loaded...")
 
 @client.command()
 async def unload(ctx, extension):
-	client.unload_extension(f'cogs.{extension}')
-	await ctx.send("cogs loaded...")
-
+		client.unload_extension(f'cogs.{extension}')
+		await ctx.send("cogs loaded...")
 
 @client.command()
 async def reload(ctx, extension):
-	client.unload_extension(f'cogs.{extension}')
-	client.load_extension(f'cogs.{extension}')
-	await ctx.send("cogs loaded...")
-
+		client.unload_extension(f'cogs.{extension}')
+		client.load_extension(f'cogs.{extension}')
+		await ctx.send("cogs loaded...")
 
 for filename in os.listdir("./cogs"):
 	if filename.endswith(".py"):
 		client.load_extension(f'cogs.{filename[:-3]}')
 
-client.run('ur token here')
+client.run('token')
